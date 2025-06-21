3c0cf3b9234d9c20864217f76b2a1a8492b95305
kandadave <kandachumo@gmail.com>
2025-06-21 09:24:53 +0300

    fetch

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(booksJson => {
+      renderBooks(booksJson);
+
+      if (booksJson.length >=5) {
+        console.log('The 5th book in the series:', booksJson[4].name);
+      }
+
+      let totalPages = 0; 
+      booksJson.forEach(book => {
+        totalPages += book.numberOfPages;
+      });
+      console.log('Total number of pages of all books:', totalPages);
+    })
+
+    
 }
 
 function renderBooks(books) {
